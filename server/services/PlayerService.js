750ea38a56b5380d7be13e8f5fdcb132751010f3
molsen <matthewo@porch.com>
2015-02-06 14:57:21 -0800

    POSTGRES CONNECTION!!!

--- server/services/PlayerService.js
+++ server/services/PlayerService.js
@@ -3,6 +3,12 @@
 module.exports = {
     name: 'playerService',
     getPlayerById: function (request, response) {
+
+        var db = require('../db.js');
+        db.query('SELECT $1::int AS number', ['1'], function (err, result) {
+            console.log(result.rows[0].number);
+        });
+
         response.send({
             player_key: 1,
             name: 'molsen',
